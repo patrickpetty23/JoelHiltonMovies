d28f445e57a680d0aa50aad94c2dab0d06d94768
Patrick Petty <InductedCloud76@xboxlivefamily.com>
2024-02-23 23:45:16 -0700

    commenting code

--- JoelHiltonMovies/Controllers/HomeController.cs
+++ JoelHiltonMovies/Controllers/HomeController.cs
@@ -1,106 +1,126 @@
+// Import necessary namespaces.
 using JoelHiltonMovies.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
+// Define the namespace and class for the controller.
 namespace JoelHiltonMovies.Controllers
 {
+    // Define the HomeController class inheriting from Controller.
     public class HomeController : Controller
     {
+        // Define a private field to hold the database context.
         private NewMovieContext _context;
 
-        public HomeController(NewMovieContext context) 
-        { 
+        // Define a constructor to inject the database context.
+        public HomeController(NewMovieContext context)
+        {
             _context = context;
         }
 
+        // Define action method for the Index view.
         public IActionResult Index()
         {
             return View();
         }
 
+        // Define action method for the GetToKnowJoel view.
         public IActionResult GetToKnowJoel()
         {
             return View();
         }
 
+        // Define HTTP GET action method for the NewMovie view.
         [HttpGet]
         public IActionResult NewMovie()
         {
+            // Fetch categories from the database and pass them to the view.
             ViewBag.Categories = _context.Categories
                 .OrderBy(x => x.CategoryName)
                 .ToList();
 
+            // Return the NewMovie view with a new Movies object.
             return View(new Movies());
         }
 
+        // Define HTTP POST action method for the NewMovie view.
         [HttpPost]
         public IActionResult NewMovie(Movies response)
         {
+            // If model state is valid, add the movie to the database and redirect to confirmation view.
             if (ModelState.IsValid)
             {
                 _context.Movies.Add(response);
                 _context.SaveChanges();
-
                 return View("Confirmation");
             }
-            else
+            else // If model state is not valid, reload the NewMovie view with validation errors.
             {
                 ViewBag.Categories = _context.Categories
                 .OrderBy(x => x.CategoryName)
                 .ToList();
-
                 return View();
             }
-            
         }
 
+        // Define action method for the DisplayMovie view.
         public IActionResult DisplayMovie()
         {
+            // Fetch movies from the database along with their categories and order by title.
             var movies = _context.Movies
                 .Include(m => m.Category)
                 .OrderBy(m => m.Title)
                 .ToList();
 
+            // Return the DisplayMovie view with the list of movies.
             return View(movies);
-
         }
 
+        // Define HTTP GET action method for the Edit view.
         [HttpGet]
         public IActionResult Edit(int id)
         {
+            // Fetch the movie record to edit from the database.
             var record = _context.Movies
                 .Single(x => x.MovieId == id);
-            
+
+            // Fetch categories from the database and pass them to the view.
             ViewBag.Categories = _context.Categories
                 .OrderBy(x => x.CategoryName)
                 .ToList();
 
+            // Return the NewMovie view with the movie record to edit.
             return View("NewMovie", record);
         }
 
+        // Define HTTP POST action method for the Edit view.
         [HttpPost]
         public IActionResult Edit(Movies updatedInfo)
         {
+            // Update the movie record in the database and redirect to DisplayMovie view.
             _context.Update(updatedInfo);
             _context.SaveChanges();
-
             return RedirectToAction("DisplayMovie");
         }
 
+        // Define action method for the Delete view.
         public IActionResult Delete(int id)
         {
+            // Fetch the movie record to delete from the database.
             var record = _context.Movies
                 .Single(x => x.MovieId == id);
 
+            // Return the Delete view with the movie record to delete.
             return View(record);
         }
 
+        // Define HTTP POST action method for the Delete view.
         [HttpPost]
         public IActionResult Delete(Movies movie)
         {
+            // Remove the movie record from the database and redirect to DisplayMovie view.
             _context.Movies.Remove(movie);
             _context.SaveChanges();
-
             return RedirectToAction("DisplayMovie");
         }
     }
--- JoelHiltonMovies/Program.cs
+++ JoelHiltonMovies/Program.cs
@@ -1,3 +1,4 @@
+// Create a new instance of the web application builder.
 using JoelHiltonMovies.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,30 +6,44 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add the NewMovieContext to the service container, configuring it to use SQLite.
 builder.Services.AddDbContext<NewMovieContext>(options =>
 {
     options.UseSqlite(builder.Configuration["ConnectionStrings:JoelHiltonMovieConnection"]);
 });
 
+// Build the application.
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+// Check if the application is not running in development mode.
 if (!app.Environment.IsDevelopment())
 {
+    // Use the error handler middleware to redirect to the Error action method in HomeController.
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
+    // Enable HTTP Strict Transport Security (HSTS).
     app.UseHsts();
 }
 
+// Redirect HTTP requests to HTTPS.
 app.UseHttpsRedirection();
+
+// Serve static files (e.g., HTML, CSS, JavaScript).
 app.UseStaticFiles();
 
+// Enable routing.
 app.UseRouting();
 
+// Enable authorization.
 app.UseAuthorization();
 
+// Map the default controller route.
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+// Run the application.
 app.Run();
